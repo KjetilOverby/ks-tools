cc474bcfa8f9ac0c7b6d7ca6ec9c3bfb98385ce8
KjetilOverby <kjeoever@msn.com>
2023-12-18 15:26:23 +0100

    User

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,20 +1,42 @@
-import { log } from "console";
-import React from "react";
-import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
-const page = async () => {
-  const session = await getServerAuthSession();
-  const users = await api.users.getUsers.query({});
+interface User {
+  id: string;
+  name: string | null;
+  email: string | null;
+  emailVerified: Date | null;
+  image: string | null;
+  role: "ADMIN" | "LOGIN" | "MO_ADMIN" | "KV_ADMIN"; // Define the possible role values
+}
 
-  console.log(users);
+const page = async ({}: User) => {
+  const users = await api.users.getUsers.query({});
 
   return (
     <>
       <div className="p-5">
         {users.map((user) => {
+          let rolle = "";
+
+          const rolleHandeler = () => {
+            if (user.role === "ADMIN") {
+              rolle = "Utvikler";
+            } else if (user.role === "LOGIN") {
+              rolle = "Ingen rolle enda";
+            } else if (user.role === "MO_ADMIN") {
+              rolle = "Moelven Østerdalsbruket";
+            } else if (user.role === "KV_ADMIN") {
+              rolle = "Kvarnstrands & Stridbergs";
+            }
+          };
+
+          rolleHandeler();
+
           return (
-            <div className="w-96 rounded-2xl bg-accent pb-5 pl-5 ">
+            <div
+              key={user.id}
+              className="mb-5 w-96 rounded-2xl bg-accent pb-5 pl-5"
+            >
               <div className="avatar">
                 <div className="w-24 rounded-full ring ring-primary ring-offset-2 ring-offset-base-100">
                   <img src={user.image} />
@@ -23,7 +45,7 @@ const page = async () => {
               <div>
                 <p>Brukernavn: {user.name}</p>
                 <p>Email: {user.email}</p>
-                <p>Rolle: {user.role === "ADMIN" ? "Utviker" : ""}</p>
+                <p>Rolle: {rolle}</p>
               </div>
             </div>
           );
